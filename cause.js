781fa15c615371e240e4f5886d3e093175944b1f
adyaa20 <adichavan969@gmail.com>
2026-06-05 06:11:08 -0400

    Update cause.js

--- cause.js
+++ cause.js
@@ -1,22 +1,22 @@
  // Reasons database
  const reasons = [
     { 
-        text: "You’re such a kind and wonderful person, and I feel lucky to share such a good bond with you. 💖", 
+        text: "Meri aku sabse achi he  💖", 
         emoji: "🌟",
         gif: "gif1.gif"
     },
     { 
-        text: "May your day be filled with love, laughter, and endless joy. 🌸 ", 
+        text: "Tumhara din hamesha haste , muskurate aur acha bite. 🌸 ", 
         emoji: "💗",
         gif: "gif2.gif"
     },
     { 
-        text: "Wishing you success, happiness, and everything your heart desires. ✨ ", 
+        text: "Tumhe jo chiye wo tumhe mil jaye. ✨ ", 
         emoji: "💕",
         gif: "gif1.gif"
     },
     { 
-        text: "Stay the amazing girl you are—always spreading positivity around. Have the happiest year ahead! 🥳 ", 
+        text: "Ese hi sabki jidagi me khushi dete rehena aku ! 🥳 ", 
         emoji: "🌟",
         gif: "gif2.gif"
     }
